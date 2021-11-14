d034ad1c7171ab7942fe70e93933d2a0b33b4876
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-14 13:21:42 -0300

    pop, push, isEmpty and top

---
+++ Estrutura de no/Pila/src/one/digital/innovation/Node.java
@@ -0,0 +1,37 @@
+package one.digital.innovation;
+
+public class Node {
+	
+	private int data;
+	private Node referenceNodeNext;
+	
+	public Node(int data) {
+		this.data = data;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getReferenceNodeNext() {
+		return referenceNodeNext;
+	}
+
+	public void setReferenceNodeNext(Node referenceNoNext) {
+		this.referenceNodeNext = referenceNodeNext;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data + "]";
+	}
+
+	
+	
+	
+
+}
---
+++ Estrutura de no/Pila/src/one/digital/innovation/Pill.java
@@ -0,0 +1,5 @@
+package one.digital.innovation;
+
+public class Pill {
+
+}
