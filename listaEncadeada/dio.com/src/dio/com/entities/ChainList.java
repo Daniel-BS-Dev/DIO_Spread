a8607f2ae11ab9ec57d0cad60ea75f147509ccc7
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-15 08:50:41 -0300

    add

--- listaEncadeada/dio.com/src/dio/com/entities/ChainList.java
+++ listaEncadeada/dio.com/src/dio/com/entities/ChainList.java
@@ -3,4 +3,30 @@ package dio.com.entities;
 public class ChainList<T> {
 
 	private Node<T> inputReference;
+
+	public ChainList() {
+		this.inputReference = null;
+	}
+	
+	public int size() {
+		int sizeList = 0;
+		Node<T> refAux = inputReference;
+		while(true) {
+			if(refAux != null) {
+				sizeList ++;
+				if(refAux.getNextNode() != null) {
+					refAux = refAux.getNextNode();
+				}else {
+					break;
+				}
+			}else {
+				break;
+			}
+		}
+		return sizeList;
+	}
+	
+	public boolean isEmpty() {
+		return inputReference == null ? true : false;
+	}
 }
