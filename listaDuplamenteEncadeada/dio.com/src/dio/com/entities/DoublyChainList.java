8b3fbf35cbcea6ed2db04071ae5b97b33c37e22c
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-15 11:00:07 -0300

    getNode, get and add

--- listaDuplamenteEncadeada/dio.com/src/dio/com/entities/DoublyChainList.java
+++ listaDuplamenteEncadeada/dio.com/src/dio/com/entities/DoublyChainList.java
@@ -1,5 +1,50 @@
 package dio.com.entities;
 
-public class DoublyChainList {
+public class DoublyChainList<T> {
+	
+	private DoubleNode<T> firstNode;
+	private DoubleNode<T> lastNode;
+	
+	private int sizeList;
+	
+	public DoublyChainList() {
+		this.firstNode = null;
+		this.lastNode = null;
+		this.sizeList = 0;
+	}
+	
+	public void add(T element) {
+		DoubleNode<T> newNode = new DoubleNode<>(element);
+		newNode.setNextNode(null);
+		newNode.setPreviousNode(lastNode);
+		
+		if(lastNode == null) {
+			firstNode = newNode;
+		}
+		if(lastNode != null) {
+			lastNode.setNextNode(newNode);
+		}
+		
+		lastNode = newNode;
+		sizeList++;
+	}
+	
+	public int size() {
+		return this.sizeList;
+	}
+	
+	public T get(int index) {
+		return this.getNode(index).getContent();
+	}
+	
+	private DoubleNode<T> getNode(int index){
+		DoubleNode<T> auxNode = firstNode;
+		
+		for(int i=0; (i < index) && (auxNode != null); i++) {
+			auxNode = auxNode.getNextNode();
+		}
+		
+		return auxNode;
+	}
 
 }
