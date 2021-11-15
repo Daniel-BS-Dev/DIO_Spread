bf32a523753589a07166bd2bafef75d1872ffc7f
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-15 13:03:51 -0300

    using Class CicularNode

--- lista circular/dio.com/src/dio/com/Main.java
+++ lista circular/dio.com/src/dio/com/Main.java
@@ -1,9 +1,28 @@
 package dio.com;
 
+import dio.com.entities.CircularList;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
+		CircularList<String> myCircularList = new CircularList<>();
+	
+		myCircularList.add("First");
+		myCircularList.add("Second");
+		myCircularList.add("Third");
+		myCircularList.add("Fourth");
+		myCircularList.add("Fifth");
+	
+		System.out.println(myCircularList);
+		
+		myCircularList.remove(0);
+		
+		System.out.println(myCircularList);
+		
+		System.out.println(myCircularList.get(3));
+		
+		System.out.println(myCircularList.size());
 	}
 
 }
