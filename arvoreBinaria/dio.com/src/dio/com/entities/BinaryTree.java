30eab4fc1ee4829fd19a0b81171e390a1d1bba51
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-15 15:25:46 -0300

    show in order, pos order and pre order

--- arvoreBinaria/dio.com/src/dio/com/entities/BinaryTree.java
+++ arvoreBinaria/dio.com/src/dio/com/entities/BinaryTree.java
@@ -25,10 +25,49 @@ public class BinaryTree<T extends Comparable<T>> {
 		BinaryNode<T> newNode = new BinaryNode(content);
 	    root = insert(root, newNode);
 	}
+	
+	private void showInOrder(BinaryNode<T> currentNode) {
+		if(currentNode != null) {
+			showInOrder(currentNode.getLeftNode());
+			System.out.println(currentNode.getContent()+ ", ");
+			showInOrder(currentNode.getRightNode());
+		}
+	}
+	
+	public void showInOrder() {
+		System.out.println("\n Exibindo em ordem ");
+		showInOrder(this.root);
+	}
 
+	private void showPosOrder(BinaryNode<T> currentNode) {
+		if(currentNode != null) {
+			showPosOrder(currentNode.getLeftNode());
+			showPosOrder(currentNode.getRightNode());
+			System.out.println(currentNode.getContent()+ ", ");
+		}
+	}
+	
+	public void showPosOrder() {
+		System.out.println("\n Exibindo pos ordem ");
+		showPosOrder(this.root);
+	}
+	
+	private void showPreOrder(BinaryNode<T> currentNode) {
+		if(currentNode != null) {
+			System.out.println(currentNode.getContent()+ ", ");
+			showPreOrder(currentNode.getLeftNode());
+			showPreOrder(currentNode.getRightNode());
+			
+		}
+	}
+	
+	public void showPreOrder() {
+		System.out.println("\n Exibindo pre ordem ");
+		showPreOrder(this.root);
+	}
 	@Override
 	public String toString() {
-		return "BinaryNode " + content;
+		return "BinaryNode ";
 	}
 	
 	
