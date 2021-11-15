9cc71fd42146d62255b946c7383bb786b70f46db
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-15 12:26:36 -0300

    created getNode and get

--- lista circular/dio.com/src/dio/com/entities/CircularList.java
+++ lista circular/dio.com/src/dio/com/entities/CircularList.java
@@ -3,12 +3,12 @@ package dio.com.entities;
 public class CircularList<T> {
 	
 	private Node<T> head;
-	private Node<T> syrup;
+	private Node<T> tail;
 	private int sizeList;
 	
 	public CircularList() {
 		this.head = null;
-		this.syrup = null;
+		this.tail = null;
 		this.sizeList = 0;
 	}
 	
@@ -20,4 +20,21 @@ public class CircularList<T> {
 		return this.sizeList == 0 ? true : false;
 	}
 
+	private Node<T> getNode(int index){
+		if(this.isEmpty()) 
+			throw new IndexOutOfBoundsException("A lista esta vazia");
+		if(index == 0) 
+			return this.tail;
+		
+		Node<T> auxNode = this.tail;
+		for(int i=0; (i < index) && (auxNode != null); i++) {
+			auxNode = auxNode.getNextNode();
+		}
+		
+		return auxNode;
+	}
+	
+	public T get(int index){
+		return this.getNode(index).getContent();
+	}
 }
