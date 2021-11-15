76d8ad00241f345ee09ec666099bc8d90d3149df
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-15 09:47:49 -0300

    remove and get

---
+++ listaEncadeada/dio.com/src/dio/com/Main.java
@@ -0,0 +1,16 @@
+package dio.com;
+
+import dio.com.entities.ChainList;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	   ChainList<String> chainList = new ChainList<>();
+	   
+	   chainList.add("first");
+	   chainList.add("first");
+		
+	}
+
+}
--- listaEncadeada/dio.com/src/dio/com/entities/ChainList.java
+++ listaEncadeada/dio.com/src/dio/com/entities/ChainList.java
@@ -8,6 +8,60 @@ public class ChainList<T> {
 		this.inputReference = null;
 	}
 	
+	public void add(T content) {
+		Node<T> newNode = new Node<>(content);
+		if(this.isEmpty()) {
+			inputReference = newNode;
+			return;
+		}
+		
+		Node<T> noAux = inputReference;
+		for(int i=0;i< this.size()-1; i++) {
+			noAux = noAux.getNextNode();
+		}
+		noAux.setNextNode(newNode);
+	}
+	
+	public T remove(int index) {
+		Node<T> nodePivor = this.getNodeList(index);
+		if(index == 0) {
+			inputReference = nodePivor.getNextNode();
+			return nodePivor.getContent();
+		}
+		
+		Node<T> nodeBefore = getNodeList(index - 1);
+		nodeBefore.setNextNode(nodePivor.getNextNode());
+		return nodePivor.getContent();
+		
+	}
+	
+	public T get(int index) {
+		return getNodeList(index).getContent();
+	}
+	
+	private Node<T> getNodeList(int index){
+		
+		validIndex(index);
+		
+		Node<T> nodeAux = inputReference;
+		Node<T> nodeReturn = null;
+		
+		for(int i=0; i< this.size()-1; i++) {
+			nodeReturn = nodeAux;
+			nodeAux = nodeAux.getNextNode();
+		}
+		
+		return nodeReturn;
+				
+	}
+	
+	private void validIndex(int index) {
+		if(index >= size()) {
+			int lastIndex = size() - 1;
+			throw new IndexOutOfBoundsException("index não exite na lista, tamanho da lista "+ lastIndex);
+		}
+	}
+	
 	public int size() {
 		int sizeList = 0;
 		Node<T> refAux = inputReference;
